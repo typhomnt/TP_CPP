42d7c7d7bda7dd9e278ce1ffb7f0d12db2ac0140
Omid Ghorreshi <omid.ghorreshi@ensimag.grenoble-inp.fr>
2015-03-01 14:41:47 +0100

    ajout d'un test des parentheses
    On teste l'ecriture en utilisant les parentheses.

---
+++ TP2_ghorreso_garcia1/src/testParenthesesLecture.cpp
@@ -0,0 +1,15 @@
+#include "Dvector.h"
+#include <fstream>
+#include <string>
+#include <cassert>
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+int main(){
+	Dvector v(5,3.5);
+	for (int i = 0; i < v.size(); i++) {
+		assert(v(i) == 3.5);
+	}
+	cout << "TEST DES PARENTHESES (LECTURE) : OK" << endl;
+}
