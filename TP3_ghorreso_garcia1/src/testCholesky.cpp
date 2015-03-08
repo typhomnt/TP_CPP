b7e918818a196f3b0eec84c324a82818d094d314
garcia1 <maxime.garcia1@ensimag.grenoble-inp.fr>
2015-03-08 09:57:21 +0100

    confirmé que cholesky marche mais prob soit avec la multsoit avec l'égalité

--- TP3_ghorreso_garcia1/src/testCholesky.cpp
+++ TP3_ghorreso_garcia1/src/testCholesky.cpp
@@ -12,14 +12,18 @@ int main() {
 	l(0,1) = 0;
 	Dmatrix lt(l);
 	lt.transpose();
-	Dmatrix a(l*lt);
-	//a = a.cholesky();
+	Dmatrix a(2,2);
+	a(0,0)=4;
+	a(0,1)=4;
+	a(1,0)=4;
+	a(1,1)=8;
+	a = a.cholesky();
 	for(int i = 0 ; i < 2 ; i++)
 		for(int j = 0 ; j < 2 ; j++){
-			//assert(a(i,j) == l(i,j));
-			cout << a(i,j) << endl;
+			assert(a(i,j) == l(i,j));
+			/*cout << a(i,j) << endl;
 			cout << "l" << endl;
-		        cout << l(i,j) << endl;
+		        cout << l(i,j) << endl;*/
 		}
 	cout << "TEST CHOLESKY : OK" << endl;
 }
